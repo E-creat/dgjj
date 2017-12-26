cf62a502351ed35af4d53e178b74f84e25390937
E-creat <1145027012@qq.com>
2017-12-26 21:22:47 +0800

    剑法3

--- src/dgjj/Dgjj.java
+++ src/dgjj/Dgjj.java
@@ -4,7 +4,10 @@ public class Dgjj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dugjj!!!!");
+		System.out.println("dugjj!!!!ooooooo");
+		System.out.println("dugjj!!!!ooooooo");
+		System.out.println("dugjj!!!!ooooooo");
+		System.out.println("dugjj!!!!ooooooo");
 	}
 
 }
