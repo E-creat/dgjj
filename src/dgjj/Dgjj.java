e820b42a2e3cfc1bbf8f191e83098d9d6ce4e7a9
w-creat <990305489@qq.com>
2017-12-26 21:16:38 +0800

    更新剑法2

--- src/dgjj/Dgjj.java
+++ src/dgjj/Dgjj.java
@@ -5,6 +5,8 @@ public class Dgjj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dugjj!!!!");
+		System.out.println("dugjj!!!!");
+		System.out.println("dugjj!!!!");
 	}
 
 }
